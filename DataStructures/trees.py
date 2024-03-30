9595aac5e7d354c7d02716814596ac4c0e775db5
Sri Anjaneyam <mr.anjaneyam@gmail.com>
2024-03-30 12:27:51 +0530

    Update trees.py

--- DataStructures/trees.py
+++ DataStructures/trees.py
@@ -9,12 +9,52 @@ class Tree:
         self.root = None
     def addNode(self, value, root=self.root):
         newNode = Node(value)
-        if self.root == None:
-            self.root = newNode
+        if root == None:
+            root = newNode
         else:
-            if value>self.root.data:
-                if self.root.right is None:
-                    self.root.right = newNode
+            if value>root.data:
+                if root.right is None:
+                    root.right = newNode
                 else:
-                    self.addNode(value, self.root.right)
-                
+                    self.addNode(value, root.right)
+            else:
+                if root.left == None:
+                    root.left = newNode
+                else:
+                    self.addNode(value, root.left)
+
+    def deleteNode(self, root, value):
+        if root is None:
+            return None
+        else:
+            if value>root.value:
+                self.deleteNode(value, root.right)
+            elif value<root.value:
+                self.deleteNode(value, root.left)
+            else:
+                if root.left is None and root.right is None:
+                    root = None
+                elif root.left is None:
+                    root = root.right
+                elif root.right is None:
+                    root = root.left
+                else:
+                    temp = self.minNode(root.right)
+                    root.data = temp.data
+                    self.deleteNode(root.right, temp.data)
+
+
+    def minNode(self, root):
+        if root.left is not None:
+            return self.minNode(root.left)
+        else:
+            return root
+    def maxNode(self, root):
+        if root.right is not None:
+            return self.maxNode(root.right)
+        else:
+            return root
+
+t = Tree()
+t.addNode(4)
+t.addNode(10)
